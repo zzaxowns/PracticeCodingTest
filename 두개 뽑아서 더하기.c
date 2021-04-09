1e95ff661cc37a4a2bba2a8089ea9912f1e7cff9
KimTaeJun <zzaxowns@naver.com>
2021-04-10 02:00:15 +0900

    Update 두개 뽑아서 더하기.c
    두개 뽑아서 더하기 완료

--- 두개 뽑아서 더하기.c
+++ 두개 뽑아서 더하기.c
@@ -2,12 +2,10 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool deplicate_Check(int* answer, int answer_len, int check_num) { // 중복 체크 함수: 중복 - true/ 중복x - false
 
-	for (int i = 0; i < answer_len; i++) {
-		if (answer[i] == check_num) {
-			return true;
-		}
+bool check_deplicate(int* answer, int answer_len, int number) { // 들어온 값이 배열 안에 값과 중복 되는지 체크
+	for (int i = 0; i < answer_len; i++) {                           // 중복 = true , 중복 안됨 = false
+		if (answer[i] == number) { return true; }
 	}
 	return false;
 }
@@ -26,22 +24,29 @@ void bubble_Sort(int* answer, int answer_len) { // 버블 정렬
 	}
 }
 
-
 // numbers_len은 배열 numbers의 길이입니다.
 int* solution(int numbers[], size_t numbers_len) {
 	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-	int* answer = (int*)malloc(sizeof(int));
-	int answer_len = 0; // 현재 배열의 길이
+	int* answer = (int*)malloc(sizeof(int) * 200);
+	//int current = 0;
+	int answer_len = 0;
+	int number = 0; // 체크해야 하는 값
 
-	for (int i = 0; i < numbers_len; i++) {
+	for (int i = 0; i < numbers_len - 1; i++) {
 		for (int j = i + 1; j < numbers_len; j++) {
-			if (!deplicate_Check(answer, answer_len, numbers[i] + numbers[j])) {
-				answer[answer_len] = numbers[i] + numbers[j];
-				answer_len++;
+
+			number = numbers[i] + numbers[j];
+
+			if (!check_deplicate(answer, answer_len, number)) {
+				answer[answer_len++] = number;
+
 			}
 		}
-
 	}
+
+	//for(int i=0; i< answer_len; i++){
+	//    printf(" %d",answer[i]);
+	//}
 
 	bubble_Sort(answer, answer_len);
 

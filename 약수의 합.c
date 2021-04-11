b005b3b6aacb5010ef05680e90e7ae93f83e3498
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-04-11 17:27:26 +0900

    약수의 합 완성
    약수의 합 완성 ... 이게 4점이라니...

---
+++ 약수의 합.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+	int answer = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (n%i == 0) {
+			answer += i;
+		}
+	}
+
+	return answer;
+}

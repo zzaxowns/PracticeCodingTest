95c5bfb179a972c01c4679929af48edb8528989e
KimTaeJun <zzaxowns@naver.com>
2021-04-12 18:52:29 +0900

    평균 구하기 추가
    평균 구하기 추가합니다.

---
+++ Level1/평균 구하기.c
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+	double answer = 0;
+	int len = arr.size();
+
+	for (int i = 0; i < len; i++) {
+		answer += arr[i];
+	}
+
+	answer /= len;
+
+	return answer;
+}

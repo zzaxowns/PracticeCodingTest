e253915c505a386c77de898342106715b25f4907
KimTaeJun <zzaxowns@naver.com>
2021-04-13 02:46:24 +0900

    문자열을 정수로 바꾸기 정정
    string 자체를 특정 자료형으로 바꾸는 함수가 존재했습니다. stoi라는 함수를 통해서 바꿔봤습니다.

--- Level1/문자열을 정수로 바꾸기.c
+++ Level1/문자열을 정수로 바꾸기.c
@@ -1,27 +1,38 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(string s) {
+//	int answer = 0;
+//	int len = s.size();
+//	int x = 1;
+//
+//	for (int i = len - 1; i > 0; i--) {
+//		answer += (s[i] - 48) * x;
+//		x *= 10;
+//	}
+//
+//	if (s[0] == '-' || s[0] == '+') {
+//		if (s[0] == '-')
+//			answer *= -1;
+//	}
+//	else {
+//		answer += (s[0] - 48) *x;
+//	}
+//
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(string s) {
-	int answer = 0;
-	int len = s.size();
-	int x = 1;
-
-	for (int i = len - 1; i > 0; i--) {
-		answer += (s[i] - 48) * x;
-		x *= 10;
-	}
-
-	if (s[0] == '-' || s[0] == '+') {
-		if (s[0] == '-')
-			answer *= -1;
-	}
-	else {
-		answer += (s[0] - 48) *x;
-	}
-
-
-	return answer;
+    int answer = stoi(s);
+    return answer;
 }
 
+//가장 간단한 방법이자 stoi라는 함수 -- string to int / string to float/ long int / double 등 다양한 함수가 있었음

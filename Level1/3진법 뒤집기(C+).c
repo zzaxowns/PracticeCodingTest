183dea05506f83a2e92ac2f83c6288229575acff
KimTaeJun <zzaxowns@naver.com>
2021-04-12 19:12:02 +0900

    3진법 뒤집기 다른 방법
    3진법 뒤집기 원래 방법에서 개선된 방법을 찾아서 추가했습니다.

--- Level1/3진법 뒤집기(C+).c
+++ Level1/3진법 뒤집기(C+).c
@@ -1,6 +1,31 @@
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int solution(int n) {
+//	int answer = 0;
+//	vector<int> v;
+//	int x = 1; // 10진법으로 바꿀 때 3의 거듭수를 만들기 위한 변수
+//
+//	while (n) { //num이 int여서 0.~~ 가 나오는 순간 0으로 처리됨
+//		v.push_back(n % 3);
+//		n /= 3;
+//	}
+//
+//	reverse(v.begin(), v.end());
+//
+//	for (int i = 0; i < v.size(); i++) {
+//		answer += v[i] * x;
+//		x *= 3;
+//	}
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -14,12 +39,10 @@ int solution(int n) {
 		n /= 3;
 	}
 
-	reverse(v.begin(), v.end());
-
-	for (int i = 0; i < v.size(); i++) {
+	for (int i = v.size() - 1; i >= 0; i--) {
 		answer += v[i] * x;
 		x *= 3;
 	}
 
 	return answer;
-}
+} // 생각해보니까 reverse가 필요가 없었다..

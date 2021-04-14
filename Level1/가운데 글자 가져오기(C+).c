a102c7fc9a7630bc938b9583149ffac7255f0752
KimTaeJun <zzaxowns@naver.com>
2021-04-14 22:56:47 +0900

    가운데 글자 가져오기 다른 방법
    substr(start, len) 시작지점부터 일정 길이까지를 뽑아오는 함수

--- Level1/가운데 글자 가져오기(C+).c
+++ Level1/가운데 글자 가져오기(C+).c
@@ -1,3 +1,24 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(string s) {
+//	string answer = "";
+//	int len = s.size();
+//	int mid = len / 2;
+//
+//	if (len % 2 == 0) { // 짝수일 경우 (앞+뒤 2개)
+//		answer.push_back(s[mid - 1]);
+//		answer.push_back(s[mid]);
+//	}
+//	else { // 홀수 
+//		answer.push_back(s[mid]);
+//	}
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
 
@@ -9,12 +30,12 @@ string solution(string s) {
 	int mid = len / 2;
 
 	if (len % 2 == 0) { // 짝수일 경우 (앞+뒤 2개)
-		answer.push_back(s[mid - 1]);
-		answer.push_back(s[mid]);
+		answer = s.substr(mid - 1, 2);
 	}
 	else { // 홀수 
-		answer.push_back(s[mid]);
+		answer = s.substr(mid, 1);
 	}
 
+
 	return answer;
-}
+} // substr(시작, 시작부터 길이) 지정해서 넣는법

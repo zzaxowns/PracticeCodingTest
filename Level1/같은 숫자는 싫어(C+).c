fb46a91a45ca225e788fa1daf96ed3146616fb91
KimTaeJun <zzaxowns@naver.com>
2021-04-16 01:08:45 +0900

    같은 숫자는 싫어 업데이트
    unique 함수와 erase함수를 이용해서 더 빠르고 간단하게 푸는 방법 업로드

--- Level1/같은 숫자는 싫어(C+).c
+++ Level1/같은 숫자는 싫어(C+).c
@@ -1,18 +1,34 @@
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> arr)
+//{
+//	vector<int> answer;
+//	int len = arr.size();
+//
+//	for (int i = 0; i < len; i++) {
+//		if (answer.empty() || arr[i] != answer.back()) {
+//			answer.push_back(arr[i]);
+//		}
+//	}
+//
+//	return answer;
+//}
+
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> arr)
 {
-	vector<int> answer;
-	int len = arr.size();
 
-	for (int i = 0; i < len; i++) {
-		if (answer.empty() || arr[i] != answer.back()) {
-			answer.push_back(arr[i]);
-		}
-	}
+	arr.erase(unique(arr.begin(), arr.end()), arr.end());
 
+	vector<int> answer = arr;
 	return answer;
 }
+// erase(시작, 끝) 지우기 
+// unique를 이용해서 정렬시키고 나오는 중복되는 부분부터 end까지 지우게 만든 코드 

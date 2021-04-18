f9d68ed9d52c03b67ecdb3c04e48a7e6021d8815
KimTaeJun <zzaxowns@naver.com>
2021-04-19 01:19:54 +0900

    Update 가장 큰 수.c
    가장 큰 수 간단하게 정리 완료

--- Level2/가장 큰 수.c
+++ Level2/가장 큰 수.c
@@ -1,7 +1,40 @@
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//bool check_max(string str1, string str2) {
+//	return str1 + str2 > str2 + str1;
+//}
+//
+//string solution(vector<int> numbers) {
+//	string answer = "";
+//	int len = numbers.size();
+//	int clear = 0;
+//
+//	for (int i = len - 1; i >= 0; i--) {
+//		for (int j = len - 1; j >= clear + 1; j--) {
+//			if (check_max(to_string(numbers[j]), to_string(numbers[j - 1]))) {
+//				int temp = numbers[j - 1];
+//				numbers[j - 1] = numbers[j];
+//				numbers[j] = temp;
+//			}
+//		}
+//
+//		clear++;
+//	}
+//
+//	for (int i = 0; i < len; i++) {
+//		answer += to_string(numbers[i]);
+//	}
+//
+//	return answer;
+//} // 시간 초과로 실패하는 부분 있음
+
 #include <string>
 #include <vector>
-#include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 bool check_max(string str1, string str2) {
@@ -10,24 +43,21 @@ bool check_max(string str1, string str2) {
 
 string solution(vector<int> numbers) {
 	string answer = "";
-	int len = numbers.size();
-	int clear = 0;
+	vector<string> test;
 
-	for (int i = len - 1; i >= 0; i--) {
-		for (int j = len - 1; j >= clear + 1; j--) {
-			if (check_max(to_string(numbers[j]), to_string(numbers[j - 1]))) {
-				int temp = numbers[j - 1];
-				numbers[j - 1] = numbers[j];
-				numbers[j] = temp;
-			}
-		}
-
-		clear++;
+	for (auto num : numbers) {
+		test.push_back(to_string(num));
 	}
 
-	for (int i = 0; i < len; i++) {
-		answer += to_string(numbers[i]);
+	sort(test.begin(), test.end(), check_max);
+
+	for (auto num : test) {
+		answer += num;
+	}
+
+	if (test.at(0) == "0") {
+		answer = "0";
 	}
 
 	return answer;
-} // 시간 초과로 실패하는 부분 있음
+}

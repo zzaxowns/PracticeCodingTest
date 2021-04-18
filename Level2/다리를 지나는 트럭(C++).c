c647c7a12668dc5baf32aea0bf4d35bc850d5d22
KimTaeJun <zzaxowns@naver.com>
2021-04-18 22:06:47 +0900

    다리 지나는 트럭 완료
    다리 지나는 트럭 완료. 다른 사람들 풀이도 비슷한 느낌

--- Level2/다리를 지나는 트럭(C++).c
+++ Level2/다리를 지나는 트럭(C++).c
@@ -7,37 +7,37 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
 	int answer = 0;
-	stack<int> wait;
-	queue<int> buffer;
-	int len = truck_weights.size();
-	int current_weight = 0;
+	stack<int> wait;   // 대기 차량
+	queue<int> buffer; // 다리 공간 버퍼 
+	int len = truck_weights.size(); // 스택 초기화 변수
+	int current = 0;    //다리에 현재 무게 변수
 
-	for (int i = len; i >= 0; i--) {
+	for (int i = len - 1; i >= 0; i--) { //대기 차량 초기화 
 		wait.push(truck_weights[i]);
 	}
 
-	while (!(buffer.empty() && wait.empty())) {
+	while (!wait.empty()) {
 
-		if ((current_weight + wait.top() < weight) // 새로운 트럭이 들어갈 수 있을 때
-			|| buffer.size() < bridge_length) {
-			current_weight += wait.top();
+		if (buffer.size() >= bridge_length) { // 다리 공간 버퍼가 오버되면 맨 앞에 차량 빼주기
+			current -= buffer.front();
+			buffer.pop();
+		}
+
+		if (current + wait.top() <= weight) { // 다리에 중량이 초과되지 않으면 대기 차량 보내기
 			buffer.push(wait.top());
-
+			current += wait.top();
 			wait.pop();
 		}
-		else { // 새로운 트럭이 들어갈 수 없을 때 0을 추가해서 큐를 밀어준다.
-			if (current_weight != 0) {
-				buffer.push(0);
-
-				if (buffer.size() > bridge_length) {// 앞에 트럭이 밀려서 지나감
-					int temp = buffer.front();
-					current_weight -= temp;
-					buffer.pop();
-				}
-			}
+		else { // 중량이 초과될 경우 '0'이라는 가상의 차량으로 버퍼 채우기
+			buffer.push(0);
 		}
 
-		answer++;
+		answer++; // 사이클이 돌 때마다 시간++
+
+		if (wait.empty()) { // 만약에 대기 차량이 없다 = 마지막 차량이 달리기 시작 = 다리의 값만큼 시간 필요 
+			answer += bridge_length;
+			break;
+		}
 
 	}
 

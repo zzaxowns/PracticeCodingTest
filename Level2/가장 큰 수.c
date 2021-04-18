4414a77503e4b768f1f2559d7798caa8049b2f09
KimTaeJun <zzaxowns@naver.com>
2021-04-19 00:52:18 +0900

    Create 가장 큰 수.c
    돌아는 가는데 시간 초과되는 부분을 처리해야함

---
+++ Level2/가장 큰 수.c
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool check_max(string str1, string str2) {
+	return str1 + str2 > str2 + str1;
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	int len = numbers.size();
+	int clear = 0;
+
+	for (int i = len - 1; i >= 0; i--) {
+		for (int j = len - 1; j >= clear + 1; j--) {
+			if (check_max(to_string(numbers[j]), to_string(numbers[j - 1]))) {
+				int temp = numbers[j - 1];
+				numbers[j - 1] = numbers[j];
+				numbers[j] = temp;
+			}
+		}
+
+		clear++;
+	}
+
+	for (int i = 0; i < len; i++) {
+		answer += to_string(numbers[i]);
+	}
+
+	return answer;
+} // 시간 초과로 실패하는 부분 있음

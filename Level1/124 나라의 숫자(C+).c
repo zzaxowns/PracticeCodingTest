0866907e126831543fbd339f337a8302493044ed
KimTaeJun <zzaxowns@naver.com>
2021-04-25 04:04:54 +0900

    Update 124 나라의 숫자(C+).c
    어떻게 풀어야할지 감은 잡았는데 아직 미완성

--- Level1/124 나라의 숫자(C+).c
+++ Level1/124 나라의 숫자(C+).c
@@ -4,6 +4,7 @@
 using namespace std;
 
 int check_num(int num) {
+
 	if (num == 0) {
 		return 4;
 	}
@@ -13,12 +14,30 @@ int check_num(int num) {
 
 string solution(int n) {
 	string answer = "";
+	vector<int> temp;
+	int len = 0;
 
 	while (n > 0) {
-		int num = check_num(n % 3);
-		answer = to_string(num) + answer;
+		if (n < 4) {
+			temp.push_back(n);
+			len++;
+			n = 0;
+		}
+		else {
+			if (temp[len] >= 3 || temp.empty()) {
+				temp.push_back(1);
+				len++;
+			}
+			else {
+				temp[len]++;
+			}
 
-		n /= 3;
+			n -= 3;
+		}
+	}
+
+	for (int i = 0; i < len; i++) {
+		answer = to_string(temp[i]) + answer;
 	}
 
 	return answer;

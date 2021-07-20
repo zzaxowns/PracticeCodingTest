ae005bf0714d47667ad77edf241332cb9862c61c
KimTaeJun <zzaxowns@naver.com>
2021-07-20 17:01:14 +0900

    Update 해시 1레벨 완주하지 못한 선수.c
    해시로 푸는 방법 업데이트

--- Level1/해시 1레벨 완주하지 못한 선수.c
+++ Level1/해시 1레벨 완주하지 못한 선수.c
@@ -1,23 +1,45 @@
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//string solution(vector<string> participant, vector<string> completion) {
+//	string answer = "";
+//	int participant_Len = participant.size();
+//
+//	//µÎ º¤ÅÍ Á¤·Ä
+//	sort(participant.begin(), participant.end());
+//	sort(completion.begin(), completion.end());
+//
+//	for (int i = 0; i < participant_Len; i++) {
+//		if (participant[i] != completion[i]) {
+//			answer = participant[i];
+//			break;
+//		}
+//
+//	}
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
 	string answer = "";
-	int participant_Len = participant.size();
+	unordered_map<string, int> table;
 
-	//µÎ º¤ÅÍ Á¤·Ä
-	sort(participant.begin(), participant.end());
-	sort(completion.begin(), completion.end());
+	for (auto& i : participant) { table[i]++; }
+	for (auto& i : completion) { table[i]--; }
 
-	for (int i = 0; i < participant_Len; i++) {
-		if (participant[i] != completion[i]) {
-			answer = participant[i];
-			break;
+	for (auto& i : table) {
+		if (i.second > 0) {
+			answer = i.first;
 		}
-
 	}
 
 	return answer;

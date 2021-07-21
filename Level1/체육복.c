e6e434bb9d740bfbf65dc2058e8fb7c96c63d313
KimTaeJun <zzaxowns@naver.com>
2021-07-22 01:51:40 +0900

    Update 체육복.c
    조건 하나 생각을 안해서 추가했음. 하지만 아직 완성 안됨

--- Level1/체육복.c
+++ Level1/체육복.c
@@ -1,7 +1,37 @@
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int solution(int n, vector<int> lost, vector<int> reserve) {
+//	int lost_Num = 0;
+//	unordered_map<int, int> table;
+//
+//	for (auto& num : lost) { table[num]--; }
+//
+//	for (auto& num : reserve) {
+//		if (table[num - 1] == -1) {
+//			table[num - 1]++;
+//		}
+//		else if (table[num + 1] == -1) {
+//			table[num + 1]++;
+//		}
+//	}
+//
+//	for (auto& num : table) {
+//		if (num.second == -1) {
+//			lost_Num++;
+//		}
+//	}
+//
+//	return n - lost_Num;
+//}
+
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
 
 using namespace std;
 
@@ -12,7 +42,10 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 	for (auto& num : lost) { table[num]--; }
 
 	for (auto& num : reserve) {
-		if (table[num - 1] == -1) {
+		if (table[num] == -1) {
+			table[num]++;
+		}
+		else if (table[num - 1] == -1) {
 			table[num - 1]++;
 		}
 		else if (table[num + 1] == -1) {

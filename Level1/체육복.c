0eb6bacd04099c794646efa83b2cf94247113428
KimTaeJun <zzaxowns@naver.com>
2021-07-22 02:25:19 +0900

    Update 체육복.c
    완료

--- Level1/체육복.c
+++ Level1/체육복.c
@@ -28,6 +28,39 @@
 //
 //	return n - lost_Num;
 //}
+//
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//int solution(int n, vector<int> lost, vector<int> reserve) {
+//	int lost_Num = 0;
+//	unordered_map<int, int> table;
+//
+//	for (auto& num : lost) { table[num]--; }
+//
+//	for (auto& num : reserve) {
+//		if (table[num] == -1) {
+//			table[num]++;
+//		}
+//		else if (table[num - 1] == -1) {
+//			table[num - 1]++;
+//		}
+//		else if (table[num + 1] == -1) {
+//			table[num + 1]++;
+//		}
+//	}
+//
+//	for (auto& num : table) {
+//		if (num.second == -1) {
+//			lost_Num++;
+//		}
+//	}
+//
+//	return n - lost_Num;
+//}
 
 #include <string>
 #include <vector>
@@ -40,15 +73,14 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 	unordered_map<int, int> table;
 
 	for (auto& num : lost) { table[num]--; }
+	for (auto& num : reserve) { table[num]++; }
 
 	for (auto& num : reserve) {
-		if (table[num] == -1) {
-			table[num]++;
-		}
-		else if (table[num - 1] == -1) {
+
+		if (table[num - 1] == -1 && table[num] > 0) {
 			table[num - 1]++;
 		}
-		else if (table[num + 1] == -1) {
+		else if (table[num + 1] == -1 && table[num] > 0) {
 			table[num + 1]++;
 		}
 	}

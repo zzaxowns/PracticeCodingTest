b9378dace97185e992a7d8ae0fcab88f5b925657
KimTaeJun <zzaxowns@naver.com>
2021-08-05 20:25:03 +0900

    Update 설탕 배달.c
    부루트포스 알고리즘으로 풀어본 설탕배달

--- 백준/설탕 배달.c
+++ 백준/설탕 배달.c
@@ -1,3 +1,37 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int num = 0;
+//	int count = 0;
+//	cout << "배달해야하는 설탕의 킬로그램 >>> ";
+//	cin >> num;
+//
+//	if (num < 3 || 5000 < num) {
+//		cout << "수량이 허용 범위 안에 없습니다." << endl;
+//		return 0;
+//	}
+//
+//	int max_3 = num / 3;
+//	int max_5 = num / 5;
+//
+//	for (int i = 0; i < max_3; i++) {
+//		for (int j = 0; j < max_5; j++) {
+//			if (i * 3 + j * 5 == num) { 
+//				count = i + j;
+//				cout << "설탕 봉지 [" << count << "] 개로 가능" << endl;
+//				return 0;
+//			}
+//		}
+//	}
+//
+//	if (count == 0)
+//		cout << "불가능" << endl;
+//
+//	return 0;
+//}
+
 #include<iostream>
 
 using namespace std;
@@ -13,28 +47,20 @@ int main() {
 		return 0;
 	}
 
-	while (1) {
+	int max_3 = num / 3;
+	int max_5 = num / 5;
 
-		if (num == 0) {
-			break;
-		}
-		else if (num < 3) {
-			count = -1;
-			break;
-		}
-		else if (num % 5 == 0) {
-			count += num / 5;
-			break;
-		}
-		else {
-			num -= 3;
-			count++;
+	for (int i = 0; i < max_3; i++) {
+		for (int j = 0; j < max_5; j++) {
+			if (i * 3 + j * 5 == num) {
+				count = i + j;
+				cout << "설탕 봉지 [" << count << "] 개로 가능" << endl;
+				return 0;
+			}
 		}
 	}
 
-	if (count != -1)
-		cout << "설탕 봉지 [" << count << "] 개로 가능" << endl;
-	else
+	if (count == 0)
 		cout << "불가능" << endl;
 
 	return 0;

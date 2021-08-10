585e226c1fefecbd5fd5b40a188deec6aafda815
KimTaeJun <zzaxowns@naver.com>
2021-08-10 14:37:12 +0900

    Update 설탕 배달.c
    bottom up 방식 풀이 추가

--- 백준/설탕 배달.c
+++ 백준/설탕 배달.c
@@ -74,34 +74,61 @@
 //	return 0;
 //} // 이거까지 포스팅 완료 
 
+//#include<iostream>
+//
+//using namespace std;
+//
+//int dp(int N, int count) {
+//
+//	if (N == 0) {
+//		return count;
+//	}
+//	else if (N < 3) {
+//		return -1;
+//	}
+//	else if (N % 5 == 0) {
+//		int temp = N / 5;
+//		count += temp;
+//		return count;
+//	}
+//	else {
+//		count++;
+//		dp(N - 3, count);
+//	}
+//
+//}
+//
+//int main() {
+//
+//	int N = 0;
+//	int count = 0;
+//	cout << "배달해야하는 설탕의 킬로그램 >>> ";
+//	cin >> N;
+//
+//	if (N < 3 || 5000 < N) {
+//		cout << "-1" << endl;
+//		return 0;
+//	}
+//
+//	int num = dp(N, count);
+//
+//	cout << num << endl;
+//
+//	return 0;
+//}
+
 #include<iostream>
+#include<algorithm>
+
+#define MAX 5001
 
 using namespace std;
-
-int dp(int N, int count) {
-
-	if (N == 0) {
-		return count;
-	}
-	else if (N < 3) {
-		return -1;
-	}
-	else if (N % 5 == 0) {
-		int temp = N / 5;
-		count += temp;
-		return count;
-	}
-	else {
-		count++;
-		dp(N - 3, count);
-	}
-
-}
 
 int main() {
 
 	int N = 0;
-	int count = 0;
+	int arr[MAX] = { 0, };
+
 	cout << "배달해야하는 설탕의 킬로그램 >>> ";
 	cin >> N;
 
@@ -110,9 +137,21 @@ int main() {
 		return 0;
 	}
 
-	int num = dp(N, count);
+	arr[3] = 1;
+	arr[5] = 1;
 
-	cout << num << endl;
+	for (int i = 6; i <= N; i++) {
+		if (arr[i - 3]) { arr[i] = arr[i - 3] + 1; }
+
+		if (arr[i - 5]) { arr[i] = arr[i - 5] + 1; }
+	}
+
+	if (arr[N] == 0) {
+		cout << "-1" << endl;
+	}
+	else {
+		cout << arr[N] << endl;
+	}
 
 	return 0;
 }

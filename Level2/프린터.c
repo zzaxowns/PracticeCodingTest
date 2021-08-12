9ebdcdd401b9fd6292040ceff1a469c9c8f11945
KimTaeJun <zzaxowns@naver.com>
2021-08-12 21:06:46 +0900

    Update 프린터.c
    프린터 완성

--- Level2/프린터.c
+++ Level2/프린터.c
@@ -1,32 +1,31 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
 	int answer = 0;
-	int len = priorities.size();// 이 크기만큼 체크
+	int len = priorities.size();
 	int current_Index = 0;
 
 	while (1) {
 		int temp = current_Index + len;
 
-		for (int i = current_Index; i < temp; i++) {
-			if (priorities[i] > priorities[current_Index]) {
+		for (int i = current_Index; i < temp; i++) {// 현재 저장된 index에서 벡터의 길이 만큼 체크
+			if (priorities[i%len] > priorities[current_Index]) {
 				current_Index = i % len;
-				cout << current_Index << " ";
 			}
 		}
 
-		if (current_Index == location) {
+		if (current_Index == location) { //탈출 조건
 			answer++;
 			break;
 		}
-		else {
+		else {                         //탈출 조건이 안맞으면 현재 가장 큰 값을 0으로 만들고 반복
 			answer++;
 			priorities[current_Index] = 0;
 		}
+
 	}
 
 	return answer;

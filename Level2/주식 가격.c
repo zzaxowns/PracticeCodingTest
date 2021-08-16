3797b335f6d724b82044f9420a13f5ead5d26801
KimTaeJun <zzaxowns@naver.com>
2021-08-16 17:19:39 +0900

    Update 주식 가격.c
    푸는 중

--- Level2/주식 가격.c
+++ Level2/주식 가격.c
@@ -5,5 +5,25 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
 	vector<int> answer;
+	int len = prices.size();
+
+	for (int i = 0; i < len - 1; i++) {
+		int count = 0;
+		int temp = prices[i];
+		for (int j = i; j < len; j++) {
+			if (temp < prices[j]) {
+				temp = prices[j];
+				count++;
+			}
+			else if (temp > prices[j]) {
+				count++;
+				break;
+			}
+		}
+
+		answer.push_back(count);
+	}
+
+
 	return answer;
 }

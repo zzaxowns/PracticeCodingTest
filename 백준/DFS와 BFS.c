5bac3c954f9b6bf7df76eaab9746c16535c047fc
KimTaeJun <zzaxowns@naver.com>
2021-08-22 19:02:09 +0900

    Create DFS와 BFS.c

---
+++ 백준/DFS와 BFS.c
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int N = 0;
+	int M = 0;
+
+	cin >> N;
+	if (N > 1000 || N < 0) { return 0; }
+
+	cin >> M;
+	if (M < 0 || M>10000) { return 0; }
+
+	vector<vector<int>>
+
+		return 0;
+}

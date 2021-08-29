47f75c9f9cc063dc1116de327bc4457421ab9cb8
KimTaeJun <zzaxowns@naver.com>
2021-08-29 22:19:04 +0900

    Update DFS와 BFS.c
    값을 받는 부분 까지는 완성

--- 백준/DFS와 BFS.c
+++ 백준/DFS와 BFS.c
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 	int N = 0;
 	int M = 0;
+	int V = 0;
 
 	cin >> N;
 	if (N > 1000 || N < 0) { return 0; }
@@ -13,25 +14,27 @@ int main() {
 	cin >> M;
 	if (M < 0 || M>10000) { return 0; }
 
-	vector<vector<int>> g;
+	cin >> V;
+	if (V<0 || V>N) { return 0; }
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			g[i].push_back(0);
-		}
-	}
+	vector<vector<int>> g(N, vector<int>(N, 0));
 
 	for (int i = 0; i < M; i++) {
 		int node1 = 0;
 		int node2 = 0;
 
-		cin >> node1 >> node2;
-		g[node1][node2] = 1;
+		cout << "시작노드 >>>";
+		cin >> node1;
+		cout << "끝 노드 >>>";
+		cin >> node2;
+
+		g[node1 - 1][node2 - 1] = 1;
+		g[node2 - 1][node1 - 1] = 1;
 	}
 
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			cout << " " << g[i][j];
 		}
 		cout << endl;

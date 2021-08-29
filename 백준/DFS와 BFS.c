22d7944cdb21613fc7d8b576e58fd59419b1811e
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-08-29 20:53:16 +0900

    Update DFS와 BFS.c
    제작 중

--- 백준/DFS와 BFS.c
+++ 백준/DFS와 BFS.c
@@ -13,7 +13,31 @@ int main() {
 	cin >> M;
 	if (M < 0 || M>10000) { return 0; }
 
-	vector<vector<int>>
+	vector<vector<int>> g;
 
-		return 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			g[i].push_back(0);
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		int node1 = 0;
+		int node2 = 0;
+
+		cin >> node1 >> node2;
+		g[node1][node2] = 1;
+	}
+
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cout << " " << g[i][j];
+		}
+		cout << endl;
+	}
+
+
+
+	return 0;
 }

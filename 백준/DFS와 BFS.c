c46d2c1dbcde690c0b7f12e1d279de9de409d271
KimTaeJun <zzaxowns@naver.com>
2021-08-31 01:45:06 +0900

    Update DFS와 BFS.c
    백준 DFS와 BFS 완성

--- 백준/DFS와 BFS.c
+++ 백준/DFS와 BFS.c
@@ -1,14 +1,80 @@
 #include<iostream>
 #include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
 
 using namespace std;
 
-void DFS(vector<vector<int>> graph, vector<bool> check, int start_Node) {
+void DFS(vector<vector<int>> graph, vector<bool> check, int start_Node, int size) {
+	stack<int> s;
+	vector<int> print;
 
+	int current_Node = start_Node - 1;
+	s.push(current_Node);
+	check[current_Node] = true;
+
+	while (!s.empty()) {
+		current_Node = s.top();
+
+		for (int i = 0; i < size; i++) {
+			if (graph[current_Node][i] == 1 && !check[i]) {
+				current_Node = i;
+				check[current_Node] = true;
+				s.push(i);
+				break;
+			}
+			else if (i == size - 1) {
+				print.push_back(s.top());
+				s.pop();
+			}
+		}
+
+	}
+
+	reverse(print.begin(), print.end());
+
+	for (auto node : print) {
+		cout << node + 1 << " ";
+	}
+	cout << endl;
 }
 
-void BFS(vector<vector<int>> graph, vector<bool> check, int start_Node) {
+void BFS(vector<vector<int>> graph, vector<bool> check, int start_Node, int size) {
+	queue<int> q;
+	vector<int> print;
+	int current_Node = start_Node - 1;
+	q.push(current_Node);
+	check[current_Node] = true;
 
+	while (!q.empty()) {
+		current_Node = q.front();
+
+		for (int i = 0; i < size; i++) {
+			if (graph[current_Node][i] == 1 && !check[i]) {
+				q.push(i);
+				check[i] = true;
+			}
+		}
+
+		print.push_back(q.front());
+		q.pop();
+	}
+
+	for (auto node : print) {
+		cout << node + 1 << " ";
+	}
+	cout << endl;
+}
+
+void printGraph(vector<vector<int>> graph, int size) {
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << " " << graph[i][j];
+		}
+		cout << endl;
+	}
 }
 
 int main() {
@@ -41,19 +107,9 @@ int main() {
 		g[node2 - 1][node1 - 1] = 1;
 	}
 
-	DFS(g, check_Graph, V);
+	DFS(g, check_Graph, V, N);
 
-	BFS(g, check_Graph, V);
-
-	//print 출력 부분
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << " " << g[i][j];
-		}
-		cout << endl;
-	}
-
-
+	BFS(g, check_Graph, V, N);
 
 	return 0;
 }

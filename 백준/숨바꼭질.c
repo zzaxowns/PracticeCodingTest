dfdb30d9a300a667b860ac176100e0856f6d2bd9
KimTaeJun <zzaxowns@naver.com>
2021-08-31 16:05:10 +0900

    Create 숨바꼭질.c
    푸는 중

---
+++ 백준/숨바꼭질.c
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+#define MAX 100001
+
+int DFS(vector<bool> check_Arr, int N, int V) {
+	stack<int> s;
+	int count = 1;
+
+	s.push(N);
+	check_Arr[N] = true;
+
+	while (!s.empty()) {
+		int current = s.top();
+
+		if (!check_Arr[N - 1]) {
+			s.push(N - 1);
+			check_Arr[N - 1] = true;
+		}
+		else if (!check_Arr[N * 2]) {
+			s.push(N * 2);
+			check_Arr[N * 2] = true;
+		}
+		else if (!check_Arr[N + 1]) {
+			s.push(N + 1);
+			check_Arr[N + 1] = true;
+		}
+		else
+	}
+
+}
+
+int BFS() {
+}
+
+int main() {
+	int N = 0;
+	int K = 0;
+	vector<bool> check_Arr(MAX, false);
+
+	cout << "수빈이의 위치 >>> ";
+	cin >> N;
+
+	cout << "동생의 위치 >>> ";
+	cin >> K;
+
+
+
+	return 0;
+}

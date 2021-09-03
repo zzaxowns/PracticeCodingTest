b80de69e56d374cc74238abaef8145ff467bd8e7
KimTaeJun <zzaxowns@naver.com>
2021-09-03 12:13:14 +0900

    Update 숨바꼭질.c
    숨바꼭질 완성

--- 백준/숨바꼭질.c
+++ 백준/숨바꼭질.c
@@ -7,44 +7,48 @@ using namespace std;
 
 #define MAX 100001
 
-bool Isvaild(int num) {
-	bool temp = num <= 100000 && num > 0 ? true : false;
-
-	temp = temp || num
-
-		return
+bool Isvaild(int num, const vector<bool> check) {
+	if (num <= 100000 && num > 0 && !check[num])
+		return true;
+	else
+		return false;
 }
 
-int Search(vector<bool> check_Arr, int N, int V) {
+int Search(vector<bool> check_Arr, int N, int K) {
 	queue<pair<int, int>> q;
+	int answer = -1;
+
 	q.push({ N,0 });
 	check_Arr[N] = true;
+
 
 	while (!q.empty()) {
 		pair<int, int> temp = q.front();
 
-		if (temp.first == N) { // 현재 맨 앞에 있는게 정답
-			return temp.second;
+		if (temp.first == K) { // 현재 맨 앞에 있는게 정답
+			answer = temp.second;
+			break;
 		}
 
-		if (Isvaild(temp.first - 1)) {
+		if (Isvaild(temp.first - 1, check_Arr)) {
 			q.push({ temp.first - 1, temp.second + 1 });
 			check_Arr[temp.first - 1] = true;
 		}
 
-		if (Isvaild(temp.first + 1)) {
+		if (Isvaild(temp.first + 1, check_Arr)) {
 			q.push({ temp.first + 1, temp.second + 1 });
 			check_Arr[temp.first + 1] = true;
 		}
 
-		if (Isvaild(temp.first * 2)) {
+		if (Isvaild(temp.first * 2, check_Arr)) {
 			q.push({ temp.first * 2, temp.second + 1 });
 			check_Arr[temp.first * 2] = true;
 		}
 
+		q.pop();
 	}
 
-	return
+	return answer;
 }
 
 
@@ -59,7 +63,7 @@ int main() {
 	cout << "동생의 위치 >>> ";
 	cin >> K;
 
-
+	cout << Search(check_Arr, N, K) << endl;
 
 	return 0;
 }

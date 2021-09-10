1ca65f273277b10587c11adaa8c7c4d394af06dc
KimTaeJun <zzaxowns@naver.com>
2021-09-10 18:09:31 +0900

    Update 섬 연결하기.c
    문제점 찾는 중

--- Level3/섬 연결하기.c
+++ Level3/섬 연결하기.c
@@ -1,12 +1,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include<iostream>
-#include <unordered_map>
-
-#define MAX 99999999
-
 using namespace std;
+
+int find_parent(int n, const int* arr) { // 부모 노드 찾기
+	return n == arr[n] ? n : find_parent(arr[n], arr);
+}
+
+bool check_parent(int n1, int n2, const int* arr) { // 부모노드가 같은지 확인
+	int node1 = find_parent(n1, arr);
+	int node2 = find_parent(n2, arr);
+
+	return node1 == node2 ? true : false;
+}
 
 bool comp(vector<int> n1, vector<int> n2) {
 	return n1[2] < n2[2];
@@ -14,24 +20,21 @@ bool comp(vector<int> n1, vector<int> n2) {
 
 int solution(int n, vector<vector<int>> costs) {
 	int answer = 0;
-	int count = 0;
-	unordered_map<int, bool> check;
+	int check_arr[101];
+
+	for (int i = 0; i < 101; i++) { check_arr[i] = i; }
 
 	sort(costs.begin(), costs.end(), comp);
 
-	for (auto cases : costs) { // 방문 노드 초기화 
-		check[cases[0]] = false;
-		check[cases[1]] = false;
-	}
-
 	for (int i = 0; i < costs.size(); i++) {
-		int temp = check[costs[i][0]] & check[costs[i][1]];
+		int start = costs[i][0];
+		int end = costs[i][1];
 
-		if (!temp) {
+		if (!check_parent(start, end, check_arr)) {
 			answer += costs[i][2];
-			check[costs[i][0]] = true;
-			check[costs[i][1]] = true;
+			check_arr[end] = start;
 		}
+
 	}
 
 	return answer;

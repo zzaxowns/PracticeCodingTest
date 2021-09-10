1025fee0e1b4df6d3688cf7b094155b8c04b5def
KimTaeJun <zzaxowns@naver.com>
2021-09-10 22:06:19 +0900

    Update 섬 연결하기.c
    크루스칼 알고리즘을 이용한 섬 연결하기 완. 서로의 부모를 엮어줄 때 바로 엮는 것이 아니고 각 요소의 루트를 찾아서 연결해주는 것이 중요하다.

--- Level3/섬 연결하기.c
+++ Level3/섬 연결하기.c
@@ -1,17 +1,12 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 int find_parent(int n, const int* arr) { // 부모 노드 찾기
 	return n == arr[n] ? n : find_parent(arr[n], arr);
-}
-
-bool check_parent(int n1, int n2, const int* arr) { // 부모노드가 같은지 확인
-	int node1 = find_parent(n1, arr);
-	int node2 = find_parent(n2, arr);
-
-	return node1 == node2 ? true : false;
 }
 
 bool comp(vector<int> n1, vector<int> n2) {
@@ -27,11 +22,12 @@ int solution(int n, vector<vector<int>> costs) {
 	sort(costs.begin(), costs.end(), comp);
 
 	for (int i = 0; i < costs.size(); i++) {
-		int start = costs[i][0];
-		int end = costs[i][1];
+		int start = find_parent(costs[i][0], check_arr);
+		int end = find_parent(costs[i][1], check_arr);
 
-		if (!check_parent(start, end, check_arr)) {
+		if (start != end) {
 			answer += costs[i][2];
+			cout << costs[i][2] << " ";
 			check_arr[end] = start;
 		}
 

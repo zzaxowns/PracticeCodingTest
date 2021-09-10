2c4b1676c7d9257ff598bbd5a0170c6a7cf9d0e6
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-10 15:50:55 +0900

    Update 섬 연결하기.c

--- Level3/섬 연결하기.c
+++ Level3/섬 연결하기.c
@@ -1,31 +1,53 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
-#define MAX 99999
+#define MAX 99999999
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> costs) {
 	int answer = 0;
 	int count = 0;
-	vector<bool> check(n, false);
+	unordered_map<int, bool> check;
 
-	for (int j = 0; j < n - 1; j++) {
+	for (int i = 0; i < costs.size(); i++) {
+		check[costs[i][0]] = false;
+		check[costs[i][1]] = false;
+	}
+
+	while (1) {
 		int min = 0;
 
 		for (int i = 0; i < costs.size(); i++) {
-			bool temp = check[costs[i][0]] && check[costs[i][1]] ? true : false;
-
-			if (!temp && (costs[i][2] < costs[min][2])) {
+			if (costs[i][2] < costs[min][2]) {
 				min = i;
 			}
 		}
 
-		answer += costs[min][2];
-		check[costs[min][0]] = true;
-		check[costs[min][1]] = true;
-		costs[min][2] = MAX;
+		bool temp = check[costs[min][0]] && check[costs[min][1]] ? true : false;
+
+		if (temp) {
+			costs[min][2] = MAX;
+		}
+		else {
+			answer += costs[min][2];
+			check[costs[min][0]] = true;
+			check[costs[min][1]] = true;
+			costs[min][2] = MAX;
+		}
+
+		temp = true;
+
+		for (auto ch : check) {
+			if (!ch.second) {
+				temp = ch.second;
+				break;
+			}
+		}
+
+		if (temp) { break; }
 	}
 
 	return answer;

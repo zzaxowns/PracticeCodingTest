b537ccff1230d2e6723eb4f1e11a29592a0b9444
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-15 11:47:00 +0900

    Update 수박수박수박수박수박.c
    비트 연산자로 홀짝을 가려낼 수 있는 방법 추가

--- Level1/수박수박수박수박수박.c
+++ Level1/수박수박수박수박수박.c
@@ -1,18 +1,34 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(int n) {
+//	string answer = "수";
+//
+//	for (int i = 1; i < n; i++) {
+//		bool even = i % 2 == 0 ? true : false;
+//
+//		if (even)
+//			answer += "수";
+//		else
+//			answer += "박";
+//	}
+//
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
 string solution(int n) {
-	string answer = "수";
+	string answer = "";
 
-	for (int i = 1; i < n; i++) {
-		bool even = i % 2 == 0 ? true : false;
-
-		if (even)
-			answer += "수";
-		else
-			answer += "박";
+	for (int i = 0; i < n; i++) {
+		i & 1 ? answer += "박" : answer += "수";
 	}
 
 

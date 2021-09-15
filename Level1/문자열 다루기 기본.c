bae66c2b8f35e6874daa07a5c1a97361c6b03a98
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-15 11:30:01 +0900

    Update 문자열 다루기 기본.c
    isdigit 이라는 함수를 사용해서 배열의 요소가 숫자인지 판단 가능

--- Level1/문자열 다루기 기본.c
+++ Level1/문자열 다루기 기본.c
@@ -1,3 +1,29 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//bool solution(string s) {
+//	bool answer = true;
+//	int len = s.size();
+//
+//	if (!(len == 4 || len == 6)) {
+//		answer = false;
+//	}
+//	else {
+//
+//		printf("길이가 4 또는 6");
+//		for (auto ch : s) {
+//			if (ch - '0' > 9 || ch - '0' < 0) {
+//				answer = false;
+//				break;
+//			}
+//		}
+//	}
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
 
@@ -5,21 +31,12 @@ using namespace std;
 
 bool solution(string s) {
 	bool answer = true;
-	int len = s.size();
-
-	if (!(len == 4 || len == 6)) {
-		answer = false;
-	}
-	else {
-
-		printf("길이가 4 또는 6");
-		for (auto ch : s) {
-			if (ch - '0' > 9 || ch - '0' < 0) {
-				answer = false;
-				break;
-			}
+	for (auto ch : s) {
+		if (!isdigit(ch)) {
+			answer = false;
+			break;
 		}
 	}
 
-	return answer;
+	return s.size() == 4 || s.size() == 6 ? answer : false;
 }

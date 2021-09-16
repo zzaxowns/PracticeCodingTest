e22b44c2f117a9655b35cc6c8f0522d263601837
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-16 11:41:13 +0900

    Create 문자열 내림차순으로 배치하기.c
    문자열 내림 차순으로 배치하기

---
+++ Level1/문자열 내림차순으로 배치하기.c
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string s) {
+	string answer = s;
+
+	sort(answer.begin(), answer.end(), greater());
+
+	return answer;
+}

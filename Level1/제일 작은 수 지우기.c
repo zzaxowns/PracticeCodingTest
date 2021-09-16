456b496a733a2dde650cc4d7df4e6d4b20c1cbe9
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-16 11:25:44 +0900

    Update 제일 작은 수 지우기.c
    제일 작은 수 지우기 개선

--- Level1/제일 작은 수 지우기.c
+++ Level1/제일 작은 수 지우기.c
@@ -1,3 +1,21 @@
+//#include <string>
+//#include <vector>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> arr) {
+//	vector<int> answer;
+//	int min = *min_element(arr.begin(), arr.end());
+//
+//	for (auto num : arr) {
+//		if (min != num)
+//			answer.push_back(num);
+//	}
+//
+//	if (answer.empty()) { answer.push_back(-1); }
+//	return answer;
+//}
 #include <string>
 #include <vector>
 #include<algorithm>
@@ -5,14 +23,10 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-	vector<int> answer;
-	int min = *min_element(arr.begin(), arr.end());
+	vector<int> answer = arr;
+	int min_index = min_element(arr.begin(), arr.end()) - arr.begin();
 
-	for (auto num : arr) {
-		if (min != num)
-			answer.push_back(num);
-	}
+	answer.erase(answer.begin() + min_index);
 
-	if (answer.empty()) { answer.push_back(-1); }
-	return answer;
+	return answer.empty() ? vector<int>(1, -1) : answer;
 }

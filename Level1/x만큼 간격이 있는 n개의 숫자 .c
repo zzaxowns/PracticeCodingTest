13df2a1377b8a447ff10dbbc53486acc8a04c36b
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-16 11:40:19 +0900

    문제 2개 추가
    레벨 1짜리 문제 처리 중

---
+++ Level1/x만큼 간격이 있는 n개의 숫자 .c
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+	vector<long long> answer;
+
+	for (int i = 1; i <= n; i++) {
+		answer.push_back(x*i);
+	}
+
+	return answer;
+}

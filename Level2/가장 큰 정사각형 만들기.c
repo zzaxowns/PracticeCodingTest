c3aaa31bb52c8d90f5c879496328bb2623cd5026
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-19 14:16:35 +0900

    Update 가장 큰 정사각형 만들기.c
    푸는 방식은 같은데 세세한 조건 부분을 변경함

--- Level2/가장 큰 정사각형 만들기.c
+++ Level2/가장 큰 정사각형 만들기.c
@@ -7,13 +7,14 @@ using namespace std;
 int solution(vector<vector<int>> board)
 {
 	int answer = 0;
-	int height = board.size() - 1;
-	int width = board[0].size() - 1;
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
+	int height = board.size();
+	int width = board[0].size();
+
+	for (int i = 1; i < height; i++) {
+		for (int j = 1; j < width; j++) {
 			if (board[i][j]) {
-				board[i + 1][j + 1] = 1 + min({ board[i][j],board[i + 1][j], board[i][j + 1] });
-				answer = max(answer, board[i + 1][j + 1]);
+				board[i][j] = 1 + min({ board[i - 1][j],board[i][j - 1], board[i - 1][j - 1] });
+				answer = max(answer, board[i][j]);
 			}
 		}
 	}

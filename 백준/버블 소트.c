974b58109ac13ca6a13434827761645f3ee54faa
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-09-25 18:16:25 +0900

    Create 버블 소트.c

---
+++ 백준/버블 소트.c
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int Size,answer;
+vector<int> arr;
+
+void Input() {
+	cin >>Size;
+
+	for (int i = 0; i < Size; i++) {
+		int temp = 0;
+		cin >> temp;
+		arr.push_back(temp);
+	}
+}
+
+void bubble() {
+	for (int i = 0; i < Size; i++) {
+		for (int j = 0; j < Size-1; j++) {
+			if (arr[j + 1] < arr[j]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				answer++;
+			}
+		}
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << answer<<endl;
+
+	return 0;
+}

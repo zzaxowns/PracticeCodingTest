ade7494388133c1a943e66f5dabd0c7f9979f7bc
KimTaeJun <zzaxowns@naver.com>
2021-09-30 23:30:52 +0900

    Update 124 나라의 숫자(C+).c
    완

--- Level1/124 나라의 숫자(C+).c
+++ Level1/124 나라의 숫자(C+).c
@@ -3,41 +3,23 @@
 
 using namespace std;
 
-int check_num(int num) {
-
-	if (num == 0) {
-		return 4;
-	}
-
-	return num;
-}
-
 string solution(int n) {
 	string answer = "";
-	vector<int> temp;
-	int len = 0;
 
-	while (n > 0) {
-		if (n < 4) {
-			temp.push_back(n);
-			len++;
-			n = 0;
+	while (n != 0) {
+		int temp = n % 3;
+		n /= 3;
+
+		if (temp == 0) {
+			answer = '4' + answer;
+			n--;
 		}
-		else {
-			if (temp[len] >= 3 || temp.empty()) {
-				temp.push_back(1);
-				len++;
-			}
-			else {
-				temp[len]++;
-			}
-
-			n -= 3;
+		else if (temp == 1) {
+			answer = '1' + answer;
 		}
-	}
-
-	for (int i = 0; i < len; i++) {
-		answer = to_string(temp[i]) + answer;
+		else if (temp == 2) {
+			answer = '2' + answer;
+		}
 	}
 
 	return answer;

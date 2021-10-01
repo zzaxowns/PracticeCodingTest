3910c7029e2e7d8e523aeb55f0963d13dbdbbc50
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-01 15:36:59 +0900

    숫자의 표현 머지
    머지 테스트

--- Level2/숫자의 표현.c
+++ Level2/숫자의 표현.c
@@ -6,7 +6,6 @@ using namespace std;
 int solution(int n) {
 	int answer = 0;
 
-<<<<<<< HEAD
 	answer++;
 
 	for (int i = n / 2 + 1; i > 0; i--) {
@@ -20,9 +19,6 @@ int solution(int n) {
 
 		if (sum == n) { answer++; }
 	}
-=======
-
->>>>>>> b08bd1abd612a0d9fa4624cc494d8d28f786ffb7
 
 	return answer;
 }

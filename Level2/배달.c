41e34b4becb3fb1133719f93b8016a7ac8ea2c28
KimTaeJun <zzaxowns@naver.com>
2021-10-05 00:00:30 +0900

    Update 배달.c

--- Level2/배달.c
+++ Level2/배달.c
@@ -3,16 +3,20 @@
 using namespace std;
 
 int solution(int N, vector<vector<int> > road, int K) {
-	int answer = -1;
-	vector<int> v(999999, N);
-	v[0] = 0;
+	int answer = 0;
+	vector<int> v(N + 1, 999999);
+	v[1] = 0;
 	int len = road.size();
 
-	for (int i = 1; i < len; i++) {
+	for (int i = 0; i < len; i++) {
 		int min_Id = v[road[i][0]] < v[road[i][1]] ? road[i][0] : road[i][1];
 		int max_Id = v[road[i][0]] > v[road[i][1]] ? road[i][0] : road[i][1];
 
 		v[max_Id] = v[max_Id] > (v[min_Id] + road[i][2]) ? (v[min_Id] + road[i][2]) : v[max_Id];
+	}
+
+	for (auto w : v) {
+		cout << w << " ";
 	}
 
 	for (auto w : v) {

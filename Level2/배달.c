5f140ed3d27fe23fa1c3c366f67016d0979bd4bb
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-06 18:02:20 +0900

    Update 배달.c

--- Level2/배달.c
+++ Level2/배달.c
@@ -1,27 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 using namespace std;
+
+vector<pair<int, int>> graphs[51];
+int dist[51];
+
+void dijkstra(int start) {
+	priority_queue<pair<int, int>>pq;
+
+	pq.push({ 0,start });
+	dist[start] = 0;
+
+	while (!pq.empty()) {
+		int distance = -pq.top().first;
+		int now = pq.top().second;
+
+		if (dist[now] < distance) continue;
+
+		for (int i = 0; i < graphs[now].size(); i++) {
+			int cost = distance + graphs[now][i].second;
+
+			if (cost < dist[graphs[now][i].first]) {
+				dist[graphs[now][i].first] = cost;
+				pq.push(make_pair(-cost, graphs[now][i].first));
+			}
+		}
+
+		pq.pop();
+	}
+
+}
 
 int solution(int N, vector<vector<int> > road, int K) {
 	int answer = 0;
-	vector<int> v(N + 1, 999999);
-	v[1] = 0;
-	int len = road.size();
 
-	for (int i = 0; i < len; i++) {
-		int min_Id = v[road[i][0]] < v[road[i][1]] ? road[i][0] : road[i][1];
-		int max_Id = v[road[i][0]] > v[road[i][1]] ? road[i][0] : road[i][1];
-
-		v[max_Id] = v[max_Id] > (v[min_Id] + road[i][2]) ? (v[min_Id] + road[i][2]) : v[max_Id];
+	for (int i = 0; i < road.size(); i++) { // 도착지, 가중치
+		graphs[road[i][0]].push_back(make_pair(road[i][1], road[i][2]));
 	}
 
-	for (auto w : v) {
-		cout << w << " ";
-	}
+	for (int i = 0; i < 51; i++) { dist[i] = 999999; }
 
-	for (auto w : v) {
-		if (w <= K)
-			answer++;
+	dijkstra(1);
+
+	for (int i = 0; i <= N; i++) {
+		cout << dist[i] << " ";
 	}
 
 	return answer;

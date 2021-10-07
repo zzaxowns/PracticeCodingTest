df554bd1708bf041ca4b555546f4a67702c9ebc1
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-07 15:57:37 +0900

    Update 단속카메라.c

--- Level3/단속카메라.c
+++ Level3/단속카메라.c
@@ -1,19 +1,39 @@
+#include <iostream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <stack>
 using namespace std;
 
-int solution(vector<vector<int>> triangle) {
+int solution(vector<vector<int>> routes) {
 	int answer = 0;
+	stack<pair<int, int>> s;
 
-	for (int i = triangle.size() - 1; i >= 0; i--) {
-		for (int j = 0; j < triangle[i].size() - 1; j++) {
-			int num = triangle[i][j] > triangle[i][j + 1] ? triangle[i][j] : triangle[i][j + 1];
-			triangle[i - 1][j] += num;
-		}
+	sort(routes.begin(), routes.end(), greater());
+
+	for (int i = 0; i < routes.size(); i++) {
+		s.push({ routes[i][0],routes[i][1] });
+		cout << routes[i][0] << " " << routes[i][1] << endl;
 	}
 
-	answer = triangle[0][0];
+
+
+	while (!s.empty()) {
+		pair<int, int> temp = s.top();
+		s.pop();
+
+		cout << temp.first << " " << temp.second << endl;
+
+		if (temp.second > s.top().first) {
+			pair<int, int> newTemp = { temp.first,s.top().second };
+			s.pop();
+			s.push(newTemp);
+			continue;
+		}
+
+		s.pop();
+		answer++;
+	}
 
 	return answer;
 }

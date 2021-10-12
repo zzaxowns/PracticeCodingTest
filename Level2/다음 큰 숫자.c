105f9c625eba3b1ae43c73979235ceb594695c0e
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-12 15:17:31 +0900

    Update 다음 큰 숫자.c
    다음 큰 숫자 비트 마스크 이용해서 푼 풀이 추가

--- Level2/다음 큰 숫자.c
+++ Level2/다음 큰 숫자.c
@@ -1,3 +1,33 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int binary(int num) {
+//	int count = 0;
+//
+//	while (num != 0) {
+//		if (num % 2 == 1) count++;
+//		num /= 2;
+//	}
+//
+//	return count;
+//}
+//
+//int solution(int n) {
+//	int answer = 0;
+//	int add = 1;
+//
+//	while (1) {
+//		if (binary(n) == binary(n + add))
+//			break;
+//
+//		add++;
+//	}
+//
+//	return n + add;
+//}
+
 #include <string>
 #include <vector>
 
@@ -14,12 +44,22 @@ int binary(int num) {
 	return count;
 }
 
+int beatMask(int n) {
+	int ret = 0;
+	// n의 이진수에서 1의 개수 세기
+	for (int i = 0; i < 31; i++) {
+		if (n & 0x01 << i)
+			ret++;
+	}
+	return ret;
+}
+
 int solution(int n) {
 	int answer = 0;
 	int add = 1;
 
 	while (1) {
-		if (binary(n) == binary(n + add))
+		if (binary(n) == beatMask(n + add))
 			break;
 
 		add++;

f68eca882693088efe9a60ec88fcabd2e6139c35
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-12 17:59:47 +0900

    Update 다음 큰 숫자.c

--- Level2/다음 큰 숫자.c
+++ Level2/다음 큰 숫자.c
@@ -44,14 +44,16 @@ int binary(int num) {
 	return count;
 }
 
-int beatMask(int n) {
-	int ret = 0;
-	// n의 이진수에서 1의 개수 세기
+int bitMask(int num) {
+	int count = 0;
+
 	for (int i = 0; i < 31; i++) {
-		if (n & 0x01 << i)
-			ret++;
+		if (num & 1 << i) {
+			count++;
+		}
 	}
-	return ret;
+
+	return count;
 }
 
 int solution(int n) {
@@ -59,7 +61,7 @@ int solution(int n) {
 	int add = 1;
 
 	while (1) {
-		if (binary(n) == beatMask(n + add))
+		if (binary(n) == bitMask(n + add))
 			break;
 
 		add++;

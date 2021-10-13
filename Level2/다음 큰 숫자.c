237416dad371e5b813fb10c7ba46e06f8b52f20c
KimTaeJun <zzaxowns@naver.com>
2021-10-13 23:29:15 +0900

    Update 다음 큰 숫자.c

--- Level2/다음 큰 숫자.c
+++ Level2/다음 큰 숫자.c
@@ -48,8 +48,8 @@ int bitMask(int num) {
 	int count = 0;
 
 	for (int i = 0; i < 31; i++) {
-		if (num & 1 << i) {
-			count++;
+		if (num & 1 << i) {// 각 자리수를 1과 & 연산 하고 i만큼씩 자리 이동을 하면서
+			count++;       // 참이라는 의미는 1이라는 의미가 되기 때문에 count++;
 		}
 	}
 

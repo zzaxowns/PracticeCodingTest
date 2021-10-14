db00bfaf291fda9af9d8f802bf472fa4d4adfd0c
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-14 17:24:53 +0900

    Update 카펫.c
    아.. 2차방정식 틀리면 사람이 아닌데.... 나는 사람이 아니였나보다

--- Level2/카펫.c
+++ Level2/카펫.c
@@ -6,17 +6,18 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
 	vector<int> answer;
-	int len = brown / 2;
-
+	int len = (brown + 4) / 2;
 
 	for (int i = len; i > 0; i--) {
 		bool temp = (len - i - 2) * (i - 2) == yellow ? true : false;
 
 		if (temp) {
 			answer.push_back(i);
+			answer.push_back(len - i);
 			break;
 		}
 	}
+
 
 	return answer;
 }

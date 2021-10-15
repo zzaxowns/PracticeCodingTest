e6adcb408912d06b91386248590535ab63eec6cf
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-15 17:01:54 +0900

    Create 계산기.c

---
+++ 알아두면 좋은 코드/계산기.c
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+void carculator(int n1, int n2, char op) {
+	int num = 0;
+
+
+
+}
+
+void Input(stack<int> s) {
+	string input = "";
+
+	cin >> input;
+
+}
+
+int main() {
+	stack<int> s;
+
+
+
+
+
+	return 0;
+}

dc2370ace0a2f62fa9c6ef1cd14c6431e2708fc1
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-16 17:26:48 +0900

    Update 괄호 회전하기.c
    stack 으로 풀었

--- Level2/괄호 회전하기.c
+++ Level2/괄호 회전하기.c
@@ -1,32 +1,80 @@
+//#include <string>
+//#include <vector>
+//#include <deque>
+//
+//using namespace std;
+//
+//bool checking(deque<char> arr) {
+//	int shape[3] = { 0, };
+//	int check = true;
+//
+//	for (auto ch : arr) {
+//
+//		if (ch == '(' || ch == ')')
+//			ch == '(' ? shape[0]++ : shape[0]--;
+//		else if (ch == '[' || ch == ']')
+//			ch == '[' ? shape[1]++ : shape[1]--;
+//		else if (ch == '{' || ch == '}')
+//			ch == '{' ? shape[2]++ : shape[2]--;
+//
+//		if (shape[0] < 0 || shape[1] < 0 || shape[2] < 0) {
+//			check = false;
+//			break;
+//		}
+//	}
+//
+//	if (!(shape[0] == 0 && shape[1] == 0 && shape[2] == 0))
+//		check = false;
+//
+//	return check;
+//}
+//
+//int solution(string s) {
+//	int answer = 0;
+//	deque<char> arr;
+//
+//	for (auto& ch : s) { arr.push_back(ch); }
+//
+//	for (int i = 0; i < s.size(); i++) {
+//		if (checking(arr)) { answer++; }
+//
+//		char temp = arr.front();
+//		arr.pop_front();
+//		arr.push_back(temp);
+//	}
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
 #include <deque>
+#include <stack>
 
 using namespace std;
 
-bool checking(deque<char> arr) {
-	int shape[3] = { 0, };
-	int check = true;
+bool check_stack(deque<char> arr) {
+	stack<char> s;
 
-	for (auto ch : arr) {
-
-		if (ch == '(' || ch == ')')
-			ch == '(' ? shape[0]++ : shape[0]--;
-		else if (ch == '[' || ch == ']')
-			ch == '[' ? shape[1]++ : shape[1]--;
-		else if (ch == '{' || ch == '}')
-			ch == '{' ? shape[2]++ : shape[2]--;
-
-		if (shape[0] < 0 || shape[1] < 0 || shape[2] < 0) {
-			check = false;
-			break;
+	for (int i = 0; i < arr.size(); i++) {
+		if (s.empty()) {
+			s.push(arr[i]);
+			continue;
 		}
+
+		char temp = s.top();
+
+		if (temp == '(' && arr[i] == ')')
+			s.pop();
+		else if (temp == '{' && arr[i] == '}')
+			s.pop();
+		else if (temp == '[' && arr[i] == ']')
+			s.pop();
+		else
+			s.push(arr[i]);
 	}
 
-	if (!(shape[0] == 0 && shape[1] == 0 && shape[2] == 0))
-		check = false;
-
-	return check;
+	return s.empty() ? true : false;
 }
 
 int solution(string s) {
@@ -36,7 +84,8 @@ int solution(string s) {
 	for (auto& ch : s) { arr.push_back(ch); }
 
 	for (int i = 0; i < s.size(); i++) {
-		if (checking(arr)) { answer++; }
+		if (check_stack(arr))
+			answer++;
 
 		char temp = arr.front();
 		arr.pop_front();

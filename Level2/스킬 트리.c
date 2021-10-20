c368f5b2fb7e2f34cdc66e9be8ac602754cb9766
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-20 22:47:00 +0900

    Update 스킬 트리.c
    strncmp를 이용한 풀이로 주의해야 할 점이 strncmp로 넣을 때 2개의 배열과 길이 값을 넣어주어야 하는데 2개의 배열의 포인터를 넣어주어야 함. c_str()을 이용하면 되고 참일 경우에 0을 리턴함 0을 리턴하는줄 모르고 계속 문제를 못고쳤기에 이 부분이 중요한거 같음

--- Level2/스킬 트리.c
+++ Level2/스킬 트리.c
@@ -1,3 +1,45 @@
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//int solution(string skill, vector<string> skill_trees) {
+//	int answer = 0;
+//	unordered_map<char, int> table;
+//
+//	for (int i = 41; i <= 91; i++) { table[i] = 0; }
+//
+//	int size = skill.size();
+//	for (int i = 0; i < size; i++) { table[skill[i]] += 1; }
+//
+//	size = skill_trees.size();
+//	for (int i = 0; i < size; i++) {
+//		int skill_Len = skill_trees[i].size();
+//		string str = "";
+//
+//		for (int j = 0; j < skill_Len; j++) {
+//			if (table[skill_trees[i][j]])
+//				str += skill_trees[i][j];
+//		}
+//
+//		bool flag = true;
+//
+//		for (int j = 0; j < str.size(); j++) {
+//			if (str[j] != skill[j]) {
+//				flag = false;
+//				break;
+//			}
+//		}
+//
+//		if (flag)
+//			answer++;
+//	}
+//
+//	return answer;
+//}
+
+//strncmp를 이용한 풀이
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -24,17 +66,9 @@ int solution(string skill, vector<string> skill_trees) {
 				str += skill_trees[i][j];
 		}
 
-		bool flag = true;
-
-		for (int j = 0; j < str.size(); j++) {
-			if (str[j] != skill[j]) {
-				flag = false;
-				break;
-			}
-		}
-
-		if (flag)
+		if (str == "" || strncmp(str.c_str(), skill.c_str(), str.length()) == 0) {
 			answer++;
+		}
 	}
 
 	return answer;

a84729e71edd0b5af5d5cb2b473990d92198b2fd
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-20 16:14:03 +0900

    Update 스킬 트리.c
    순서가 맞아도 선행스킬이 없으면 안됨.

--- Level2/스킬 트리.c
+++ Level2/스킬 트리.c
@@ -1,26 +1,41 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <iostream>
 
 using namespace std;
 
 int solution(string skill, vector<string> skill_trees) {
 	int answer = 0;
-	unordered_map<char, short> table;
+	int current = 0;
+	unordered_map<char, int> table;
 
-	for (int i = 41; i < 91; i++) { table[41] = 0; }
+	for (int i = 41; i < 91; i++) { table[i] = 0; }
 
 	int size = skill.size();
-	for (int i = size - 1; i >= 0; i--) { table[skill[i]] += i; }
+	for (int i = 0; i < size; i++) { table[skill[i]] += i + 1; }
 
 	size = skill_trees.size();
 	for (int i = 0; i < size; i++) {
 		int skill_Len = skill_trees[i].size();
+		current = 0;
 
+		cout << "====NEW====" << endl;
 		for (int j = 0; j < skill_Len; j++) {
+			int temp = table[skill_trees[i][j]];
 
+			if (temp != 0) {
+				cout << "current " << current << "temp " << temp << endl;
+				current = current < temp ? temp : -1;
+			}
+
+			if (current == -1) {
+				answer++;
+				cout << "불가능 스킬 트리" << current << endl;
+				break;
+			}
 		}
 	}
 
-	return answer;
+	return size - answer;
 }

086e85a40956dbe58fe924938cddc1904907a1f0
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-22 23:37:42 +0900

    Create 5주차_모음사전.c

---
+++ Level2/5주차_모음사전.c
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string word) {
+	int answer = 0;
+
+	// 모든 경우의 수를 더한다 > sort > 찾는다
+	//map이라는 자료구조로 가능한지 생각
+
+	return answer;
+}

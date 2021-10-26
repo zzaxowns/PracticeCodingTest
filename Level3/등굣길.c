2e8d4d62506f31a909bebdcd99cf5732244e2df0
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-26 16:22:12 +0900

    Update 등굣길.c
    추가 테스트 케이스 다 틀려서 문제 체크 중

--- Level3/등굣길.c
+++ Level3/등굣길.c
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -7,14 +8,19 @@ int solution(int m, int n, vector<vector<int>> puddles) {
 	vector<vector<int>> map(n, vector<int>(m, 1));
 
 	for (auto puddle : puddles) {
-		map[puddle[0]][puddle[1]] = 0;
+		map[puddle[0] - 1][puddle[1] - 1] = 0;
 	}
+
+	if (m == 1 || n == 1)
+		return 1;
 
 	for (int i = 1; i < n; i++) {
 		for (int j = 1; j < m; j++) {
 			map[i][j] = map[i][j] == 0 ? 0 : map[i - 1][j] + map[i][j - 1];
 		}
 	}
+
+
 
 	return map[n - 1][m - 1];
 }

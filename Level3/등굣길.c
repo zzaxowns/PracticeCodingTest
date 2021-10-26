7f2e18f1770f141be11b29b3c1f409f41fa25eae
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-26 16:12:33 +0900

    Update 등굣길.c
    기본 테스트 케이스 통과

--- Level3/등굣길.c
+++ Level3/등굣길.c
@@ -4,13 +4,17 @@
 using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
-	int answer = 0;
 	vector<vector<int>> map(n, vector<int>(m, 1));
 
 	for (auto puddle : puddles) {
 		map[puddle[0]][puddle[1]] = 0;
 	}
 
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j < m; j++) {
+			map[i][j] = map[i][j] == 0 ? 0 : map[i - 1][j] + map[i][j - 1];
+		}
+	}
 
-	return answer;
+	return map[n - 1][m - 1];
 }

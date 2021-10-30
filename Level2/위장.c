73b305da4c7eeb813662aa61667093e4302b9e63
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-30 16:26:54 +0900

    Update 위장.c
    예외케이스를 처리했는데, 생각하지 못한 형태가 더 있는것 같음

--- Level2/위장.c
+++ Level2/위장.c
@@ -5,15 +5,19 @@
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-	int answer = 1;
+	int answer = 0;
 	unordered_map<string, int> h;
 
 	for (auto cloth : clothes) { h[cloth[1]]++; }
 
-	for (auto sample : h) { answer *= sample.second; }
+	for (auto sample : h) { answer += sample.second; }
 
-	if (h.size() != 1)
-		for (auto sample : h) { answer += sample.second; }
+
+	if (h.size() != 1) {
+		int combination = 1;
+		for (auto sample : h) { combination *= sample.second; }
+		answer += combination;
+	}
 
 	return answer;
 }

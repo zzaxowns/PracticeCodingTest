b56a32d71eef3a3ca67fbe440ae4a9f7c57622bf
KimTaeJun <zzaxowns@naver.com>
2021-11-15 23:36:03 +0900

    Update 단어 변환.c
    세부 부분에서 문제가 되는거 같은데, 원인이 words[i] 라는건 알겠음. 바로 고칠 수 있는데 조금 더 생각하고 코드 만들어 볼 생각

--- Level3/단어 변환.c
+++ Level3/단어 변환.c
@@ -1,16 +1,16 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <iostream>
 
 using namespace std;
 
 int check_different(string str1, string str2, int len) { // 몇개가 다른지 체크
 	int temp = 0;
 
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < len; i++)
 		if (str1[i] != str2[i])
 			temp++;
-	}
 
 	return temp;
 }
@@ -21,7 +21,24 @@ int solution(string begin, string target, vector<string> words) {
 	queue<pair<string, int>> table;
 	table.push({ begin,0 });
 
+	while (!table.empty()) {
+		pair<string, int> str = table.front();
 
+		if (str.first == target) {
+			answer = str.second;
+			break;
+		}
+
+		table.pop();
+
+		for (int i = 0; i < len; i++) {
+			if (check_different(str.first, words[i], len) == 1) {
+				table.push({ words[i],str.second + 1 });
+				cout << words[i] << str.second << endl;
+				words[i] = "===";
+			}
+		}
+	}
 
 
 	return answer;

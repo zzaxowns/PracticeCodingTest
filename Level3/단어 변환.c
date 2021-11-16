7de2725e9786af132f6e48db6243ab2447600549
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-11-16 16:08:47 +0900

    Update 단어 변환.c
    기본 테스트 케이스 통과, for문의 범위를 잘못 지정해었음

--- Level3/단어 변환.c
+++ Level3/단어 변환.c
@@ -18,28 +18,28 @@ int check_different(string str1, string str2, int len) { // 몇개가 다른지 체크
 int solution(string begin, string target, vector<string> words) {
 	int answer = 0;
 	int len = begin.size();
+	int size = words.size();
+
 	queue<pair<string, int>> table;
+	vector<bool> check(words.size(), false);
 	table.push({ begin,0 });
 
 	while (!table.empty()) {
 		pair<string, int> str = table.front();
+		table.pop();
 
 		if (str.first == target) {
 			answer = str.second;
 			break;
 		}
 
-		table.pop();
-
-		for (int i = 0; i < len; i++) {
-			if (check_different(str.first, words[i], len) == 1) {
+		for (int i = 0; i < size; i++) {
+			if (check_different(str.first, words[i], len) == 1 && !check[i]) {
 				table.push({ words[i],str.second + 1 });
-				cout << words[i] << str.second << endl;
-				words[i] = "===";
+				check[i] = true;
 			}
 		}
 	}
-
 
 	return answer;
 }

670e3d1ecfbb6d234209df8287bff515c9cdd787
KimTaeJun <zzaxowns@naver.com>
2021-11-20 23:05:50 +0900

    Create 윳놀이.c
    윳놀이 코드 더 괜찮게 만들어보려는데 해시가 되네;

---
+++ 알아두면 좋은 코드/윳놀이.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	unordered_map<int, vector<int>> table;
+
+	table[0] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19 };
+
+
+	return 0;
+}

0fca3e960cabf7bac322e409e572378611408bc3
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-11-23 21:01:41 +0900

    Update 등굣길.c

--- Level3/등굣길.c
+++ Level3/등굣길.c
@@ -24,3 +24,20 @@ int solution(int m, int n, vector<vector<int>> puddles) {
 
 	return map[n - 1][m - 1];
 }
+
+//만들던 코드 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+	int answer = 0;
+	vector<vector<int>> map(n, vector<int>(m, 1));
+
+	for (auto puddle : puddles) { map[puddle[0]][puddle[1]] = 0; }
+
+
+
+	return answer;
+}

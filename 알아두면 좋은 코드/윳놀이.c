5de8a4145999b72d79ba62b2ac0dc3ed2453c46b
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-11-24 18:27:21 +0900

    Update 윳놀이.c
    윳놀이 테스트 케이스 마지막 통과. 다른 테스트 케이스도 확인해야 함

--- 알아두면 좋은 코드/윳놀이.c
+++ 알아두면 좋은 코드/윳놀이.c
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+vector<int> input = { 4,4,2,4,-1,-1 };
+
 int main() {
 	unordered_map<int, vector<int>> table;
 
@@ -13,6 +15,28 @@ int main() {
 	table[15] = { 15,16,17,18,19,0 };
 	table[22] = { 22,27,28,0 };
 
+	int current_Table = 0;
+	int current = 0;
+	int cur_Index = 0;
+	bool isBack = false;
+
+	for (auto num : input) { //{4,4,2,4,-1,-1}
+		if (!table[current].empty() && current != current_Table) {
+			if (!isBack) {
+				cur_Index = 0;
+				current_Table = current;
+			}
+			else {
+				isBack = !isBack;
+			}
+		}
+
+		isBack = num == -1 ? true : false;
+		cur_Index += num;
+		current = table[current_Table][cur_Index];
+
+		cout << current << " ";
+	}
 
 	return 0;
 }

7c294a3014ef1917a66e31e42809aceb4062b257
KimTaeJun <zzaxowns@naver.com>
2021-11-27 22:14:27 +0900

    Update 윳놀이.c
    1~4번 케이스 완료/ 5번 케이스 문제 해결 못함

--- 알아두면 좋은 코드/윳놀이.c
+++ 알아두면 좋은 코드/윳놀이.c
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <stack>
 
 using namespace std;
 
@@ -13,6 +14,7 @@ vector<vector<int>> input = { {1,-1},
 int main() {
 	unordered_map<int, vector<int>> table;
 
+
 	table[0] = { 0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,0 };
 	table[5] = { 5, 20,21,22,23,24,15,16,17,18,19,0 };
 	table[10] = { 10, 25,26,22,27,28,0 };
@@ -23,25 +25,38 @@ int main() {
 		int current_Table = 0;
 		int current = 0;
 		int cur_Index = 0;
-		bool isBack = false;
+
+		stack<pair<int, int>> recode;
 
 		for (auto num : input_arr) { //{4,4,2,4,-1,-1}
-			if (!table[current].empty() && current != current_Table) {
-				if (!isBack) {
-					cur_Index = 0;
-					current_Table = current;
-				}
-				else {
-					isBack = !isBack;
-				}
+
+			if (!table[current].empty() && current != current_Table) { // 새로운 테이블로 들어갈 때 
+				current_Table = current;
+				cur_Index = 0;
 			}
 
-			isBack = num == -1 ? true : false;
 			cur_Index += num;
+
+			if (num < 0) {
+				pair<int, int> temp = recode.top();
+				recode.pop();
+				current_Table = temp.first;
+				cur_Index = temp.second - 1;
+			}
+			else {
+				recode.push({ current_Table,cur_Index });
+			}
+
 			current = table[current_Table][cur_Index];
-			cout << "테이블: " << current_Table << " 인덱스: " << cur_Index << " 위치: " << current << endl;
+			//cout << "테이블: " << current_Table << " 인덱스: " << cur_Index << " 위치: " << current << endl;
+
+			if (current == 0)
+				break;
+
 		}
-		cout << "==================" << endl;
+		cout << current << endl;
+
+		//cout << "==================" << endl;
 	}
 
 	return 0;

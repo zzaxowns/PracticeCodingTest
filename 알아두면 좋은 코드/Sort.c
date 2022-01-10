3aae9c7b6eadbf9c3d5583dfdceffd8b0550bdc9
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2022-01-10 20:47:00 +0900

    Update Sort.c
    버블 추가

--- 알아두면 좋은 코드/Sort.c
+++ 알아두면 좋은 코드/Sort.c
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+void bubbleSort(int a[], int n) {
+	int i, j; 
+
+	for (i = 0; i < n - 1; i++)
+		for (j = 0; j < n - 1 - i; j++)
+			if (a[j] > a[j + 1])
+				swap(&a[j], &a[j + 1]);
+	return;
+}
 
 
 int main() {

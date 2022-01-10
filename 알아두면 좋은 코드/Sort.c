531b72d2fe0b0b4adb3eaf746b6cd265e6555f06
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2022-01-10 20:48:07 +0900

    Update Sort.c
    삽입정렬

--- 알아두면 좋은 코드/Sort.c
+++ 알아두면 좋은 코드/Sort.c
@@ -2,32 +2,44 @@
 
 using namespace std;
 
-void bubbleSort(int a[], int n) {
-	int i, j; 
+//void bubbleSort(int a[], int n) {
+//	int i, j; 
+//
+//	for (i = 0; i < n - 1; i++)
+//		for (j = 0; j < n - 1 - i; j++)
+//			if (a[j] > a[j + 1])
+//				swap(&a[j], &a[j + 1]);
+//	return;
+//}
 
-	for (i = 0; i < n - 1; i++)
-		for (j = 0; j < n - 1 - i; j++)
-			if (a[j] > a[j + 1])
-				swap(&a[j], &a[j + 1]);
-	return;
-}
+//void SelectionSort(int a[], int n) {
+//	int i, j;
+//	int minIndex;
+//
+//	for (i = 0; i < n - 1; i++) {
+//		minIndex = i;
+//		for (j = i + 1; j < n; j++) {
+//
+//			if (a[j] < a[minIndex])
+//				minIndex = j;
+//		}
+//
+//		if (minIndex != i)
+//			swap(&a[minIndex], &a[i]);
+//	}
+//}
 
-void SelectionSort(int a[], int n) {
-	int i, j;
-	int minIndex;
-
-	for (i = 0; i < n - 1; i++) {
-		minIndex = i;
-		for (j = i + 1; j < n; j++) {
-
-			if (a[j] < a[minIndex])
-				minIndex = j;
-		}
-
-		if (minIndex != i)
-			swap(&a[minIndex], &a[i]);
-	}
-}
+//void InsertionSort(int a[], int n) {
+//	int i, j;
+//
+//	for (i = 0; i < n; i++) {
+//		j = i;
+//		while (j > 0 && a[j - 1] > a[j]) {
+//			swap(&a[j - 1], &a[j]);
+//			j = j - 1;
+//		}
+//	}
+//}
 
 int main() {
 	

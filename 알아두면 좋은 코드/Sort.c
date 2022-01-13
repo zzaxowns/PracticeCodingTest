2316854329654c595c8b580709c4f4cd93193e5d
KimTaeJun <zzaxowns@naver.com>
2022-01-13 20:12:49 +0900

    Update Sort.c
    기수정렬 추가

--- 알아두면 좋은 코드/Sort.c
+++ 알아두면 좋은 코드/Sort.c
@@ -188,6 +188,28 @@ using namespace std;
 //	return;
 //}
 
+//기수 정렬
+//void radix_sort(int list[], int n)
+//{
+//	int i, b, d, factor = 1;
+//	QueueType queues[BUCKETS];
+//
+//	for (b = 0; b < BUCKETS; b++)
+//		init_queue(&queues[b]);
+//
+//	for (d = 0; d < DIGITS; d++) {
+//		for (i = 0; i < n; i++)
+//			enqueue(&queues[(list[i] / factor) % 10], list[i]);
+//
+//		i = 0;
+//		for (b = 0; b < BUCKETS; b++) {
+//			while (!is_empty(&queues[b])) {
+//				list[i] = dequeue(&queues[b]);
+//				i++;
+//			}
+//		}
+//		factor *= 10;
+//	}
 
 
 int main() {

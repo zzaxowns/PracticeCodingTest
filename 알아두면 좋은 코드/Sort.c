10b8ef3d58e62b80042177dd4905a7aee13dcb5d
KimTaeJun <zzaxowns@naver.com>
2022-01-13 20:11:09 +0900

    Update Sort.c
    힙 소트 추가

--- 알아두면 좋은 코드/Sort.c
+++ 알아두면 좋은 코드/Sort.c
@@ -69,6 +69,49 @@ using namespace std;
 //	return pIndex;
 //}
 
+
+//힙정렬
+//void insert_max_heap(HeapType* h, element item) {
+//	int i;
+//	i = ++(h->heap_size);
+//
+//	while ((i != 1) && (item.key > h->heap[i / 2].key)) {
+//		h->heap[i] = h->heap[i / 2];
+//		i /= 2;
+//	}
+//
+//	h->heap[i] = item;
+//	return;
+//}
+//
+//element delete_max_heap(HeapType* h) {
+//	int parent, child;
+//	element item, temp;
+//	item = h->heap[1];
+//	temp = h->heap[(h->heap_size)--];
+//	parent = 1;
+//	child = 2;
+//
+//	while (child <= h->heap_size) {
+//		// 현재 노드의 자식노드 중 더 작은 자식노드를 찾는다. 
+//		if ((child < h->heap_size) && (h->heap[child].key) < h->heap[child + 1].key)
+//			child++;
+//
+//		if (temp.key >= h->heap[child].key)
+//			break;
+//
+//		// 한 단계 아래로 이동
+//		h->heap[parent] = h->heap[child];
+//		parent = child;
+//		child *= 2;
+//	}
+//
+//	h->heap[parent] = temp;
+//	return item;
+//}
+
+
+
 int main() {
 	
 

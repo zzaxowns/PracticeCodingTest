fd0e8cda9461abf4b22804837c7210035b13f553
KimTaeJun <zzaxowns@naver.com>
2022-01-13 20:12:24 +0900

    Update Sort.c
    셸 정렬 추가

--- 알아두면 좋은 코드/Sort.c
+++ 알아두면 좋은 코드/Sort.c
@@ -160,6 +160,34 @@ using namespace std;
 //	return;
 //}
 
+//¼Ğ Á¤·Ä
+//void inc_insertion_sort(int list[], int first, int last, int gap)
+//{
+//	int i, j, key;
+//
+//	for (i = first + gap; i <= last; i = i + gap) {
+//		key = list[i];
+//		for (j = i - gap; j >= first && key < list[j]; j = j - gap)
+//			list[j + gap] = list[j];
+//		list[j + gap] = key;
+//	}
+//
+//	return;
+//}
+//
+//void shell_sort(int list[], int n)   // n: size
+//{
+//	int i, gap;
+//
+//	for (gap = n / 2; gap > 0; gap = gap / 2) {
+//		if ((gap % 2) == 0)
+//			gap++;
+//		for (i = 0; i < gap; i++)
+//			inc_insertion_sort(list, i, n - 1, gap);
+//	}
+//	return;
+//}
+
 
 
 int main() {

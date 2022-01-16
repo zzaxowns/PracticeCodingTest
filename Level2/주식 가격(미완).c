c339c6c0817bd875b1aaca4ee73c4bc2477207d4
KimTaeJun <zzaxowns@naver.com>
2022-01-16 20:35:29 +0900

    Update 주식 가격(미완).c
    세세한 부분에서 조정을 못하는 중

--- Level2/주식 가격(미완).c
+++ Level2/주식 가격(미완).c
@@ -35,17 +35,32 @@ vector<int> solution(vector<int> prices) {
 
 using namespace std;
 
+int check_Stack(stack<int> s, int num, int size) {
+	int count = 0;
+	int temp = s.top();
+
+	for (int i = 0; i < size; i++) {
+		if (temp <= s.top()) {
+			count++;
+			s.pop();
+		}
+		else
+			break;
+	}
+
+	return count;
+}
+
 vector<int> solution(vector<int> prices) {
 	vector<int> answer;
 	stack<int> s;
 
-	answer.push_front(0);
+	int len = prices.size();
 
-	int len = prices.size() - 1;
+	for (int i = len - 1; i >= 0; i++) { s.push_back(prices[i]); }
 
 	for (int i = 0; i < len; i++) {
-
-
+		answer.push_back(check_Stack(s, i, len));
 	}
 
 	return answer;

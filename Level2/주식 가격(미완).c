3b392d7d216d244ae34db8f18f56f63a09af9944
KimTaeJun <zzaxowns@naver.com>
2022-01-16 22:37:00 +0900

    Update 주식 가격(미완).c
    stack 으로 푸는 방법을 못찾겠어서 다른 사람의 풀이를 봤는데... 이걸 어떻게 이렇게 풀 생각을 했냐...

--- Level2/주식 가격(미완).c
+++ Level2/주식 가격(미완).c
@@ -28,39 +28,29 @@ vector<int> solution(vector<int> prices) {
 	return answer;
 }
 //문제가 너무 애매
-
 #include <string>
 #include <vector>
 #include <stack>
 
 using namespace std;
 
-int check_Stack(stack<int> s, int num, int size) {
-	int count = 0;
-	int temp = s.top();
-
-	for (int i = 0; i < size; i++) {
-		if (temp <= s.top()) {
-			count++;
-			s.pop();
-		}
-		else
-			break;
-	}
-
-	return count;
-}
-
 vector<int> solution(vector<int> prices) {
-	vector<int> answer;
+	vector<int> answer(prices.size());
 	stack<int> s;
-
 	int len = prices.size();
 
-	for (int i = len - 1; i >= 0; i++) { s.push_back(prices[i]); }
-
 	for (int i = 0; i < len; i++) {
-		answer.push_back(check_Stack(s, i, len));
+		while (!s.empty() && prices[s.top()] > prices[i]) {
+			answer[s.top()] = i - s.top();
+			s.pop();
+		}
+
+		s.push(i);
+	}
+
+	while (!s.empty()) {
+		answer[s.top()] = len - 1 - s.top();
+		s.pop();
 	}
 
 	return answer;
